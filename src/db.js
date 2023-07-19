ec2020172c9ef60c38d994a0cb92ce08c66f341e
JonathanFritz <fritz@nosc.io>
2023-07-19 14:15:47 +0200

    Updating driver

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import knex from "knex";
 
 const db = knex({
-  client: process.env.MAILMAN_DB_ENGINE || "mqsql",
+  client: process.env.MAILMAN_DB_ENGINE || "mysql",
   connection: {
     host: process.env.MAILMAN_DB_HOST || "127.0.0.1",
     user: process.env.MAILMAN_DB_USER,
